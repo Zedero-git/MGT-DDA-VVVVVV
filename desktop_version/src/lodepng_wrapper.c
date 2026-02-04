939cb872b41bba3f20285c73b4612c5170c456dc
Zedero <zebderoover.gamedev@gmail.com>
2026-02-04 18:03:41 +0100

    Wasm build error
    lodepng_wrapper.c needed a path change for the wasm build

--- desktop_version/src/lodepng_wrapper.c
+++ desktop_version/src/lodepng_wrapper.c
@@ -2,5 +2,6 @@
 #include <lodepng.h>
 #else
 /* Workaround for being unable to rename lodepng.cpp to lodepng.c */
-#include "../../third_party/lodepng/lodepng.cpp"
+//#include "../../third_party/lodepng/lodepng.cpp"
+#include "lodepng.cpp"
 #endif
